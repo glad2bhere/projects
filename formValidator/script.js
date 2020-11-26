2b7a2b9b8cef98b24afd0bb91ac8780c0d8adae5
Josh Hofstad <hofst038@gmail.com>
2020-11-26 16:41:13 +0100

    Finished simple form validation
    Tiddied up error messages

--- formValidator/script.js
+++ formValidator/script.js
@@ -4,10 +4,24 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
-//Show Innput Error Message
+//Show Input Error Message
 function showError(input, message) {
   const formControl = input.parentElement;
   formControl.className = 'form-control error';
+  const small = formControl.querySelector('small');
+  small.innerHTML = message;
+}
+
+//Make sure email is a valid email address
+function isValidEmail(email) {
+  const pattern =/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return pattern.test(String(email).toLowerCase);
+}
+
+//Show Success Outline
+function showSuccess(input) {
+  const formControl = input.parentElement;
+  formControl.className = 'form-control success';
 }
 
 //Event Listeners
@@ -18,6 +32,26 @@ form.addEventListener('submit', (e) => {
     showError(username, 'Username is Required');
   } else {
     showSuccess(username);
+  }
+
+  if(email.value === '') {
+    showError(email, 'Email is Required');
+  } else if(!isValidEmail(email.value)) {
+    showError(email, 'Email is Not Valid')
+  } else {
+    showSuccess(email);
+  }
+
+  if(password.value === '') {
+    showError(password, 'Password is Required');
+  } else {
+    showSuccess(password);
+  }
+
+  if(password2.value === '') {
+    showError(password2, 'Password is Required');
+  } else {
+    showSuccess(password2);
   }
 })
 
