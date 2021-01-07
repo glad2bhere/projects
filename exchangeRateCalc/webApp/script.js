6bdee352b3ad029e304d41f362c3af93e4e6eef7
Josh Hofstad <hofst038@gmail.com>
2021-01-07 20:15:53 +0100

    Exchange Rate Calc
    Having difficulties getting API to connect

---
+++ exchangeRateCalc/webApp/script.js
@@ -0,0 +1,32 @@
+const currencyEl_1 = document.getElementById('currency-one');
+const currencyEl_2 = document.getElementById('currency-two');
+const amountEl_1 = document.getElementById('amount-one');
+const amountEl_2 = document.getElementById('amount-two');
+
+const rateEl = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+//Fetch exchange rates and update DOM
+function calculate() {
+    const currency_one = currencyEl_1.value;
+    const currency_two = currencyEl_2.value;
+
+    //console.log(currencyEl_1, currencyEl_2);
+    fetch(`https://open.exchangerate-api.com/v6/latest${currency_one}`)
+        .then(res => res.json())
+        .then(data => {
+            //console.log(data);
+            const rate = data.rates[currency_two];
+
+            console.log(rate);
+        });
+}
+
+//Event listeners
+currencyEl_1.addEventListener('change', calculate);
+amountEl_1.addEventListener('input', calculate);
+currencyEl_2.addEventListener('change', calculate);
+amountEl_2.addEventListener('input', calculate);
+
+calculate();
+
